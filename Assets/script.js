b971aaff9bdd98db28164a85119e244c27f79a99
Lauryn McMichael <mcm.lacm@gmail.com>
2021-03-19 21:41:08 -0400

    initial commit

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+//variables to grab the schedule slots from 9am to 5pm
+var slot1 = document.querySelector("#nine");
+var slot2 = document.querySelector("#ten");
+var slot3 = document.querySelector("#eleven");
+var slot4 = document.querySelector("#twelve");
+var slot5 = document.querySelector("#one");
+var slot6 = document.querySelector("#two");
+var slot7 = document.querySelector("#three");
+var slot8 = document.querySelector("#four");
+var slot9 = document.querySelector("#five");
+
+//variable to grab buttons
+var saveButtons = document.querySelectorAll(".table-save");
+console.log(saveButtons);
+
+//clock function
+function clock(){
+    
+}
